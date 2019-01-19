f8a1d06ea997985cd391da970737fbf06c4790c8
Kevin Leung <kevinresol@gmail.com>
2019-01-20 00:06:59 +0800

    Add Surface

---
+++ src/react/native/paper/Surface.hx
@@ -0,0 +1,9 @@
+package react.native.paper;
+
+import react.ReactComponent;
+
+@:jsRequire('react-native-paper', 'Surface')
+extern class Surface extends ReactComponentOfProps <{
+	?style:Dynamic,
+	?children:ReactFragment,
+}> {}
