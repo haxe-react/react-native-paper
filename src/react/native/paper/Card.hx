350eb68cd15fbe80c26e8f0b7ce0cd25abc0b962
Kevin Leung <kevinresol@gmail.com>
2019-01-20 00:34:27 +0800

    Update

--- src/react/native/paper/Card.hx
+++ src/react/native/paper/Card.hx
@@ -4,5 +4,7 @@ import react.ReactComponent;
 
 @:jsRequire('react-native-paper', 'Card')
 extern class Card extends ReactComponentOfProps <{
+	?style:Dynamic,
+	?elevation:Int,
 	?children:ReactFragment,
 }> {}
--- src/react/native/paper/TextInput.hx
+++ src/react/native/paper/TextInput.hx
@@ -6,5 +6,6 @@ import react.native.component.props.*;
 @:jsRequire('react-native-paper', 'TextInput')
 extern class TextInput extends ReactComponentOfProps <{
 	> TextInputProps,
+	?mode:String,
 	?label:String,
 }> {}
